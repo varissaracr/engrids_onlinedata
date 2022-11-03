d0b5b293763640f91e888e74769b8924e1c8128f
sakda homhuan <sakda.homhuan@gmail.com>
2022-11-03 21:27:12 +0700

    update geodb

--- nodejs/service/datapull.js
+++ nodejs/service/datapull.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express.Router();
-const con = require("./db").datapool;
+const datapool = require("./db").datapool;
 
 ////formuser////
 app.post('/fuser-api/userid', (req, res) => {
     const { username, password } = req.body
-    datapull.query(`select id_user,username,form_limit,quset_limit,file_limit,img_limit from formuser where username = '${username}' and password = '${password}';`, (e, r) => {
+    datapool.query(`select id_user,username,form_limit,quset_limit,file_limit,img_limit from formuser where username = '${username}' and password = '${password}';`, (e, r) => {
         if (r.rows.length == 0) {
             res.status(200).json({
                 data: 'false'
@@ -19,7 +19,7 @@ app.post('/fuser-api/userid', (req, res) => {
 })
 app.post('/fuser-api/userid/option', (req, res) => {
     const { id_user } = req.body
-    datapull.query(`select form_limit,quset_limit,ans_limit,file_limit,img_limit,map_limit from formuser where id_user = '${id_user}';`, (e, r) => {
+    datapool.query(`select form_limit,quset_limit,ans_limit,file_limit,img_limit,map_limit from formuser where id_user = '${id_user}';`, (e, r) => {
         if (r.rows.length == 0) {
             res.status(200).json({
                 data: 'false'
@@ -35,7 +35,7 @@ app.post('/fuser-api/userid/option', (req, res) => {
 ////datasource////
 app.get('/ds-api/get', (req, res) => {
     // const { staid } = req.body
-    datapull.query(`SELECT * FROM datasource order by d_row desc;`, (e, r) => {
+    datapool.query(`SELECT * FROM datasource order by d_row desc;`, (e, r) => {
         res.status(200).json({
             data: r.rows
         })
@@ -43,7 +43,7 @@ app.get('/ds-api/get', (req, res) => {
 })
 app.get('/ds-api/getdata', (req, res) => {
     // const { staid } = req.body
-    datapull.query(`SELECT d_name,d_detail,d_groups,d_keywords,d_id,d_username,d_tnow,d_sd,d_datafiles  FROM datasource where d_access='publish' order by d_tnow desc;`, (e, r) => {
+    datapool.query(`SELECT d_name,d_detail,d_groups,d_keywords,d_id,d_username,d_tnow,d_sd,d_datafiles  FROM datasource where d_access='publish' order by d_tnow desc;`, (e, r) => {
         res.status(200).json({
             data: r.rows
         })
@@ -52,14 +52,14 @@ app.get('/ds-api/getdata', (req, res) => {
 app.post('/ds-api/save', async (req, res) => {
     const { data } = req.body;
     // console.log(data)
-    await datapull.query(`INSERT INTO datasource(d_id)VALUES('${data.d_id}');`)
-    await datapull.query(`UPDATE datasource SET d_tnow=now() WHERE d_id ='${data.d_id}';`)
+    await datapool.query(`INSERT INTO datasource(d_id)VALUES('${data.d_id}');`)
+    await datapool.query(`UPDATE datasource SET d_tnow=now() WHERE d_id ='${data.d_id}';`)
     let d;
     for (d in data) {
         if (data[d] !== '' && d !== 'd_id') {
             let sql = `UPDATE datasource SET ${d}='${data[d]}' WHERE d_id ='${data.d_id}'`;
             // console.log(sql)
-            await datapull.query(sql)
+            await datapool.query(sql)
         }
     }
     res.status(200).json({
@@ -70,13 +70,13 @@ app.post('/ds-api/save', async (req, res) => {
 app.post('/ds-api/listdata', (req, res) => {
     const { d_iduser } = req.body
     if (d_iduser !== 'administrator') {
-        datapull.query(`select d_name,d_id,d_access,d_tnow,d_sd from datasource where d_iduser='${d_iduser}' order by d_tnow desc;`, (e, r) => {
+        datapool.query(`select d_name,d_id,d_access,d_tnow,d_sd from datasource where d_iduser='${d_iduser}' order by d_tnow desc;`, (e, r) => {
             res.status(200).json({
                 data: r.rows
             })
         })
     } else {
-        datapull.query(`select d_name,d_id,d_access,d_tnow,d_sd from datasource order by d_tnow desc;`, (e, r) => {
+        datapool.query(`select d_name,d_id,d_access,d_tnow,d_sd from datasource order by d_tnow desc;`, (e, r) => {
             res.status(200).json({
                 data: r.rows
             })
@@ -85,7 +85,7 @@ app.post('/ds-api/listdata', (req, res) => {
 })
 app.post('/ds-api/editdata', (req, res) => {
     const { d_id } = req.body
-    datapull.query(`select * from datasource where d_id='${d_id}';`, (e, r) => {
+    datapool.query(`select * from datasource where d_id='${d_id}';`, (e, r) => {
         if (r.rows.length == 0) {
             res.status(200).json({
                 data: 'false'
@@ -99,7 +99,7 @@ app.post('/ds-api/editdata', (req, res) => {
 })
 app.post('/ds-api/loaddata', (req, res) => {
     const { d_id } = req.body
-    datapull.query(`select * from datasource where d_id='${d_id}';`, (e, r) => {
+    datapool.query(`select * from datasource where d_id='${d_id}';`, (e, r) => {
         if (r.rows.length == 0) {
             res.status(200).json({
                 data: 'false'
@@ -113,7 +113,7 @@ app.post('/ds-api/loaddata', (req, res) => {
 })
 app.post('/ds-api/checkdata', (req, res) => {
     const { d_id } = req.body
-    datapull.query(`select * from datasource where d_id='${d_id}' and d_access ='publish';`, (e, r) => {
+    datapool.query(`select * from datasource where d_id='${d_id}' and d_access ='publish';`, (e, r) => {
         if (r.rows.length == 0) {
             res.status(200).json({
                 data: 'false'
@@ -127,13 +127,13 @@ app.post('/ds-api/checkdata', (req, res) => {
 })
 app.post('/ds-api/update', async (req, res) => {
     const { data } = req.body
-    await datapull.query(`UPDATE datasource SET d_tnow=now() WHERE d_id ='${data.d_id}';`)
+    await datapool.query(`UPDATE datasource SET d_tnow=now() WHERE d_id ='${data.d_id}';`)
     let d;
     for (d in data) {
         if (data[d] !== '' && d !== 'd_id') {
             let sql = `UPDATE datasource SET ${d}='${data[d]}' WHERE d_id ='${data.d_id}'`;
             // console.log(sql)
-            await datapull.query(sql)
+            await datapool.query(sql)
         }
     }
     res.status(200).json({
@@ -142,7 +142,7 @@ app.post('/ds-api/update', async (req, res) => {
 })
 app.post('/ds-api/deletedata', async (req, res) => {
     const { d_id } = req.body
-    await datapull.query(`DELETE FROM datasource WHERE d_id ='${d_id}';`).then(r => {
+    await datapool.query(`DELETE FROM datasource WHERE d_id ='${d_id}';`).then(r => {
         // console.log(r.rows)
         res.status(200).json({
             data: 'success'
@@ -152,7 +152,7 @@ app.post('/ds-api/deletedata', async (req, res) => {
 app.post('/ds-api/access', async (req, res) => {
     const { d_id, d_access } = req.body
     if (d_id) {
-        await datapull.query(`UPDATE datasource SET d_access='${d_access}' WHERE d_id ='${d_id}';
+        await datapool.query(`UPDATE datasource SET d_access='${d_access}' WHERE d_id ='${d_id}';
             UPDATE datasource SET d_tpublish=now() WHERE d_id ='${d_id}';`).then(r => {
             // console.log(r.rows)
             res.status(200).json({
@@ -164,9 +164,9 @@ app.post('/ds-api/access', async (req, res) => {
 app.post('/ds-api/sd', async (req, res) => {
     const { d_id } = req.body
     if (d_id) {
-        await datapull.query(`SELECT d_sd from datasource WHERE d_id ='${d_id}';`).then(r => {
+        await datapool.query(`SELECT d_sd from datasource WHERE d_id ='${d_id}';`).then(r => {
             var sd = Number(r.rows[0].d_sd) + 1
-            datapull.query(`UPDATE datasource SET d_sd='${sd}' WHERE d_id ='${d_id}';`).then(r => {
+            datapool.query(`UPDATE datasource SET d_sd='${sd}' WHERE d_id ='${d_id}';`).then(r => {
                 res.status(200).json({
                     data: 'success'
                 })
@@ -179,7 +179,7 @@ app.post('/ds-api/sd', async (req, res) => {
 app.post('/ds-api/hitstory', async (req, res) => {
     const { data } = req.body;
     // console.log(data)
-    await datapull.query(`INSERT INTO datahitstory (id_user,username,dataid,dataname,datafile,d_tdate) 
+    await datapool.query(`INSERT INTO datahitstory (id_user,username,dataid,dataname,datafile,d_tdate) 
     VALUES('${data.id_user}','${data.username}','${data.dataid}','${data.dataname}','${data.datafile}','${data.d_tdate}');`).then(r => {
         res.status(200).json({
             data: 'hitstory'
@@ -190,7 +190,7 @@ app.post('/ds-api/hitstory/getdata', async (req, res) => {
     const { id_user } = req.body;
     // console.log(data)
     if (id_user == 'admin') {
-        await datapull.query(`SELECT username,dataname,datafile,d_tdate FROM datahitstory ORDER BY Hrow DESC;`).then(r => {
+        await datapool.query(`SELECT username,dataname,datafile,d_tdate FROM datahitstory ORDER BY Hrow DESC;`).then(r => {
             res.status(200).json({
                 data: r.rows
             })
